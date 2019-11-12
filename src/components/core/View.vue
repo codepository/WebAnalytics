0907076c21ddac679c551f5286732eec1c892c0f
mumushuiding <181457121@qq.com>
2019-11-12 15:31:48 +0800

    commit

---
+++ src/components/core/View.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-content>
+    <div id="core-view">
+      <!-- <v-fade-transition mode="out-in">
+        <router-view />
+      </v-fade-transition> -->
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"/>
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive"/>
+    </div>
+    <!-- <core-footer v-if="$route.name !== 'Maps'" /> -->
+  </v-content>
+</template>
+
+<script>
+export default {
+  metaInfo () {
+    return {
+      title: 'vuetify-admin'
+    }
+  }
+}
+</script>
+
+<style>
+#core-view {
+  padding-bottom: 50px;
+}
+</style>
